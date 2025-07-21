cc5cfc164ff9c469e1717488caa736c1fdfac0cc
hady <hady@issi.net>
2025-07-21 18:23:11 +0300

    rename

--- 5.iac/1.terraform/playground/1_provision_local-file/main.tf
+++ 5.iac/1.terraform/playground/1_provision_local-file/main.tf
@@ -2,3 +2,8 @@ resource "local_file" "file1" {
   filename = "./file1.txt"
   content  = "Hello from Terraform!"
 }
+
+resource "local_sensitive_file" "file2" {
+  filename = "./file2.txt"
+  content  = "this is very secure content!"
+}
