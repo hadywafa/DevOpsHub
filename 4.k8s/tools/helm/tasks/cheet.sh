fca7d727f19c7ffda97f8b97091a3ad42514dace
hadywafa <97hadywafa@gmail.com>
2025-11-22 13:29:59 +0200

    edit

--- 4.k8s/tools/helm/tasks/cheet.sh
+++ 4.k8s/tools/helm/tasks/cheet.sh
@@ -1,29 +1,26 @@
 # This Contain Real World Important Examples
 
+# 0.add helm repo
+helm repo list
+helm search prometheus
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus-community/kube-prometheus-stack \
+    --generate-name \
+    --namespace monitoring \
+    --create-namespace \
+    --version 45.7.1 \
+    --values values.yaml
 ## 1. Get all current installed helm charts in your cluster
-```bash
 helm list --all-namespaces --output json
-```
 
 ## 2. Get Metadata for a specific chart in your cluster
-
-
-```bash
 helm get metadata <chart_name> --namespace <namespace>
-```
 
 ## 3. Get Users Values for a specific chart
-
-```bash
 helm get values <chart_name> --namespace <namespace>
-
 helm get values prometheus-community/kube-prometheus-stack --namespace monitoring
-```
 
 ## 4. Show Default Values for a specific chart
-
-```bash
 helm show values <chart_name> --namespace <namespace>
-
-helm show values prometheus-community/kube-prometheus-stack
-```
+helm show values prometheus-community/kube-prometheus-stack --namespace monitoring
